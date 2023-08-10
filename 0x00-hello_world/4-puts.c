316751a0fb476b959e4c4e685bb111651da485b8
momen600 <momen1616@gmail.com>
2023-08-10 16:26:20 -0700

    a

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 /**
  * main - Entry point
  *
@@ -5,9 +7,8 @@
  *
  * Return: Alawys 0 (success)
 */
-
-int main (void)
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle")
 	return (0);
 }
