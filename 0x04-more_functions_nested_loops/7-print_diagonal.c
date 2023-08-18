26d6408a18b8fa470da9a1014dd109014f63c673
momen600 <momen1616@gmail.com>
2023-08-18 10:54:11 -0700

    task 2

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -18,10 +18,10 @@ void print_diagonal(int n)
 		{
 			for (number = 0; number < n; number++)
 			{
-				for (space = 0; space < number, space)
+				for (space = 0; space i< number, space)
 						_putchar(' ');
-					_putchar(92); /*is equal to '/' char*/
-					_putchar('\n');
+				_putchar(92); /*is equal to '/' char*/
+				_putchar('\n');
 			}
 		}
 }
