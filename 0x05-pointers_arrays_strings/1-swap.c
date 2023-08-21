8a929105d598abe030144a05048a057adfa08e68
momen600 <momen1616@gmail.com>
2023-08-21 08:52:09 -0700

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of two intagers
+ * using two input parameters
+ * @n: input parameter 1
+ * @n: input parameter 2
+ *
+ * Return: Nothing
+*/
+
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
