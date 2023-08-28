d775ace1133db154fe39b321436b5e909b6139c0
momen600 <momen1616@gmail.com>
2023-08-28 10:34:22 -0700

    task 1

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_diagsums - discription
+ *
+ * @a: 2d array of int types
+ * @size: size of array
+*/
+
+void print_diagsums(int *a, int size)
+{
+	int tr, d1 = 0, d2 = 0;
+
+	for (tr = 0; tr < size; tr++)
+	{
+		d1 += a[tr];
+		d2 += a[size - tr - 1];
+		a += size;
+	}
+	printf("%d, ", d1);
+	printf("%d\n, ", d2);
+}
