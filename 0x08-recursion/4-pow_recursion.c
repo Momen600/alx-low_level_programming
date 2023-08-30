603adbd29bcf8888e74bc78188d8cf2ee48ac3d3
momen600 <momen1616@gmail.com>
2023-08-30 07:48:43 -0700

    task 4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -4,7 +4,7 @@
  * _pow_recursion - search a string for any of a set of bytes.
  * @x: base
  * @y: exposant
- * Return: pointer to the byte in 's' that matches one of the vytes in 'accept'
+ * Return: pointer to the byte in 's' that matches one of the bytes in 'accept'
  * or NULL if no such byte is found
 */
 
@@ -14,7 +14,7 @@ int _pow_recursion(int x, int y)
 		return (-1);
 	else if (y == 1)
 		return (x);
-	if (y == 0)
+	else if (y == 0)
 		return (-1);
 	return (x * _pow_recursion(x, y - 1));
 }
