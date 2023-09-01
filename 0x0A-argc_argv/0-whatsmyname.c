18005654a45e81aa4708e32013c4da57c3b01075
momen600 <momen1616@gmail.com>
2023-09-01 13:41:14 -0700

    task 0 , 1

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print progrsm name, followed by a new line
+ * @argc: int
+ * @argv: list
+ * Return: 0
+*/
+
+int main(int argc, char const *aragv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main-print number of argument passed.
+ * @argc:number of commandline argument.
+ * @argv:pointer to an array of command line argument.
+ * @return:0-sucess, non-zero-fail.
+*/
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", srgc - 1);
+	return (0);
+}
