1c125b7d9cdc0c5892f4eb6721726480333e3477
momen600 <momen1616@gmail.com>
2023-09-04 14:46:15 -0700

    task 0

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,14 +1,13 @@
-#ifndef MAIN H 
+#ifndef MAIN_H
 #define MAIN_H
 
-#include <stdlib.h> 
-#include <stdio.h>
-
-int _putchar (char c) ;
 char *create_array(unsigned int size, char c);
+int _putchar(char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
+char *argstostr(int ac, char **av);
 
-#endif /* MAIN_H */
+#endif
+
