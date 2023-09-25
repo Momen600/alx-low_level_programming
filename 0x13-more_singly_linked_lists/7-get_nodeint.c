c52442c697badf571747cf154f423cdecf0cfe57
momen600 <momen1616@gmail.com>
2023-09-25 08:49:14 -0700

    a

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -13,6 +13,6 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *nod;
 	unsigned int n;
 
-	for (nod = head, n = 0, nod && n < index; nod = nod->next, n++)
+	for (nod = head, n = 0; nod && n < index; nod = nod->next, n++);
 	return (nod);
 }
